032ec6d92eede10b9a8bb487a16bb1b3ecc5bf26
Iain Reid <iainreid382@gmail.com>
2017-07-14 21:27:09 +0100

    [chore] Add some progress to the Client class

--- src/main/scala/com/zobot/Zobot.scala
+++ src/main/scala/com/zobot/Zobot.scala
@@ -2,8 +2,16 @@ package com.zobot
 
 import com.zobot.client.ZobotClient
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success}
+
 object Zobot extends App {
 
-  lazy val zobotClient = new ZobotClient("127.0.0.1", 12345)
+  val client = new ZobotClient("127.0.0.1", 12345)
+
+  client.login("user", "pass").andThen {
+    case Success(client) => client.foo()
+    case Failure(e) => println(e)
+  }
 
 }
--- src/main/scala/com/zobot/client/ZobotClient.scala
+++ src/main/scala/com/zobot/client/ZobotClient.scala
@@ -1,18 +1,67 @@
 package com.zobot.client
 
-import akka.actor.{Actor, ActorRef}
+
+import akka.actor.{Actor, ActorRef, Props}
+import akka.io.IO
+import akka.io.Tcp
+import akka.io.Tcp.{CommandFailed, Connect, Connected, Register}
 import java.net.InetSocketAddress
+
+import scala.concurrent.Future
 
 class ZobotClient(host: String, port: Int) {
 
   lazy val socketAddress = new InetSocketAddress(host, port)
+  lazy val packetClient = new PacketClient(socketAddress)
+
+  def login(username: String, password: String): Future[ZobotClient] = {
+    println(username, password)
+
+    Future.successful(this)
+  }
+
+  def foo(): Unit = {
+    println("bar")
+  }
+
 
 }
 
-private class PacketClient(remote: InetSocketAddress, listener: ActorRef) extends Actor {
+class PacketHandler extends Actor {
 
   override def receive: Receive = {
+    case data => {
+      println(data)
+    }
+  }
 
+}
+
+class PacketClient(remote: InetSocketAddress) extends Actor {
+
+  import context.system
+
+  IO(Tcp) ! Connect(remote)
+
+  lazy val listener: ActorRef = context.actorOf(Props[PacketHandler])
+
+  override def receive: Receive = {
+    case CommandFailed(_: Connect) => {
+      listener ! "Failed to connect"
+      context stop self
+    }
+
+    case connected@Connected(remote, local) => {
+      listener ! connected
+      val connection = sender()
+      connection ! Register(self)
+
+      context become {
+        case data: Any => {
+          listener ! data
+        }
+      }
+    }
   }
 
 }
