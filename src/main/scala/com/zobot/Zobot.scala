f9cbed094ec94c316cae22b207805ff58d0f5ba4
Iain Reid <iainreid382@gmail.com>
2017-07-15 22:44:04 +0100

    Create a working TCP client

--- src/main/scala/com/zobot/Zobot.scala
+++ src/main/scala/com/zobot/Zobot.scala
@@ -3,14 +3,13 @@ package com.zobot
 import com.zobot.client.ZobotClient
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.{Failure, Success}
+import scala.util.Failure
 
 object Zobot extends App {
 
-  val client = new ZobotClient("127.0.0.1", 12345)
+  val client = new ZobotClient("127.0.0.1", 8001)
 
-  client.login("user", "pass").andThen {
-    case Success(client) => client.foo()
+  client login("user", "pass") andThen {
     case Failure(e) => println(e)
   }
 
--- src/main/scala/com/zobot/client/ZobotClient.scala
+++ src/main/scala/com/zobot/client/ZobotClient.scala
@@ -1,67 +1,89 @@
 package com.zobot.client
 
-
-import akka.actor.{Actor, ActorRef, Props}
-import akka.io.IO
-import akka.io.Tcp
-import akka.io.Tcp.{CommandFailed, Connect, Connected, Register}
 import java.net.InetSocketAddress
 
-import scala.concurrent.Future
+import akka.actor.{Actor, ActorRef, ActorSystem, Props, Stash}
+import akka.io.Tcp._
+import akka.io.{IO, Tcp}
+import akka.pattern.ask
+import akka.util.{ByteString, Timeout}
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.Failure
+
+/**
+  * A class with basic interaction methods to be used against the targeted Minecraft server.
+  *
+  * @param host The host address or the server
+  * @param port The port number of the server
+  */
 class ZobotClient(host: String, port: Int) {
 
   lazy val socketAddress = new InetSocketAddress(host, port)
-  lazy val packetClient = new PacketClient(socketAddress)
 
-  def login(username: String, password: String): Future[ZobotClient] = {
+  implicit val system = ActorSystem()
+  implicit val timeout = Timeout(5 seconds)
+
+  val handler: ActorRef = system actorOf PacketHandler.props()
+  val client: ActorRef = system actorOf PacketClient.props(socketAddress, handler)
+
+  def login(username: String, password: String): Future[Unit] = {
     println(username, password)
 
-    Future.successful(this)
-  }
-
-  def foo(): Unit = {
-    println("bar")
-  }
-
-
-}
-
-class PacketHandler extends Actor {
-
-  override def receive: Receive = {
-    case data => {
-      println(data)
-    }
-  }
-
-}
-
-class PacketClient(remote: InetSocketAddress) extends Actor {
-
-  import context.system
-
-  IO(Tcp) ! Connect(remote)
-
-  lazy val listener: ActorRef = context.actorOf(Props[PacketHandler])
-
-  override def receive: Receive = {
-    case CommandFailed(_: Connect) => {
-      listener ! "Failed to connect"
-      context stop self
-    }
-
-    case connected@Connected(remote, local) => {
-      listener ! connected
-      val connection = sender()
-      connection ! Register(self)
-
-      context become {
-        case data: Any => {
-          listener ! data
-        }
+    Future {
+      client ? ByteString("hello") andThen {
+        case Failure(e) => println(e)
       }
     }
+  }
+
+  object PacketClient {
+    def props(remote: InetSocketAddress, replies: ActorRef): Props =
+      Props(new PacketClient(remote, replies))
+  }
+
+  class PacketClient(remote: InetSocketAddress, listener: ActorRef) extends Actor with Stash {
+
+    override def preStart() {
+      IO(Tcp) ! Connect(remote)
+    }
+
+    override def receive: Receive = {
+      case CommandFailed(_) =>
+        context stop self
+      case Connected(_, _) =>
+        val connection = sender()
+        connection ! Register(self)
+
+        unstashAll
+        context become {
+          case data: ByteString =>
+            connection ! Write(data)
+          case Received(data) =>
+            listener ! data
+          case _: ConnectionClosed =>
+            context stop self
+        }
+      case _: ByteString =>
+        stash
+    }
+
+  }
+
+  object PacketHandler {
+    def props(): Props =
+      Props(new PacketHandler())
+  }
+
+  class PacketHandler extends Actor {
+
+    override def receive: Receive = {
+      case data: ByteString =>
+        println(data decodeString ByteString.UTF_8)
+    }
+
   }
 
 }
