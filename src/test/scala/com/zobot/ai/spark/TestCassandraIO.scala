69afcdcb126b92cfed9ff3f146ac00de22748949
Liz Macfie <346717+Serenthia@users.noreply.github.com>
2017-07-26 01:33:23 +0100

    Add Cassandra tests

--- src/test/scala/com/zobot/ai/spark/TestCassandraIO.scala
+++ src/test/scala/com/zobot/ai/spark/TestCassandraIO.scala
@@ -9,9 +9,14 @@ class TestCassandraIO extends Specification {
                   can create new table $testCassandraCreateTable
       """
 
-  // val cass = IO.cassSession(new ContextCassandra(host = "172.17.0.2"))
+  val cass = IO.cassSession(new ContextCassandra(host = "172.17.0.2"))
 
-  def testCassandraConnection = ???
+  def testCassandraConnection = {
+    cass.getState.must_==("Connected")
+  }
 
-  def testCassandraCreateTable = ???
+  def testCassandraCreateTable = {
+    IO.cassCreateTable(cass, "activity", "test", Map("col1" -> "text", "col2" -> "UUID"))
+    cass.execute("SELECT text FROM schema_columnfamilies WHERE keyspace_name='activity'").must_==("table details")
+  }
 }
