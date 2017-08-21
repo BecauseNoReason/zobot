94fae89097462c839ba631aadb70f819572dfd08
Liz Macfie <346717+Serenthia@users.noreply.github.com>
2017-08-21 22:10:24 +0100

    Fix PlayerListItem property

--- src/main/scala/com/zobot/client/packet/definitions/clientbound/play/PlayerListItem.scala
+++ src/main/scala/com/zobot/client/packet/definitions/clientbound/play/PlayerListItem.scala
@@ -2,7 +2,7 @@ package com.zobot.client.packet.definitions.clientbound.play
 
 import com.zobot.client.packet.Packet
 
-case class PlayerListItem(action: Any, numberOfPlayers: Any, playerUuid: Any, playerAddName: Any, playerAddNumberOfProperties: Any, playerAddPropertyArrayNameString32767: Any, playerAddPropertyArrayValue: Any, playerAddPropertyArrayIsSigned: Any, playerAddPropertyArraySignature: Any, playerAddGamemode: Any, playerAddPing: Any, playerAddHasDisplayName: Any, playerAddDisplayName: Any, playerUpdateGamemode: Any, playerUpdateLatencyPing: Any, playerUpdateDisplayNameHas: Any, playerUpdateDisplayName: Any, playerRemoveNoFields: Any) extends Packet {
+case class PlayerListItem(action: Any, numberOfPlayers: Any, playerUuid: Any, playerAddName: Any, playerAddNumberOfProperties: Any, playerAddPropertyArrayName: Any, playerAddPropertyArrayValue: Any, playerAddPropertyArrayIsSigned: Any, playerAddPropertyArraySignature: Any, playerAddGamemode: Any, playerAddPing: Any, playerAddHasDisplayName: Any, playerAddDisplayName: Any, playerUpdateGamemode: Any, playerUpdateLatencyPing: Any, playerUpdateDisplayNameHas: Any, playerUpdateDisplayName: Any, playerRemoveNoFields: Any) extends Packet {
   override lazy val packetId = 0x2D
 
   override lazy val packetData: Array[Byte] =
@@ -11,7 +11,7 @@ case class PlayerListItem(action: Any, numberOfPlayers: Any, playerUuid: Any, pl
     fromAny(playerUuid) ++
     fromAny(playerAddName) ++
     fromAny(playerAddNumberOfProperties) ++
-    fromAny(playerAddPropertyArrayNameString32767) ++
+    fromAny(playerAddPropertyArrayName) ++
     fromAny(playerAddPropertyArrayValue) ++
     fromAny(playerAddPropertyArrayIsSigned) ++
     fromAny(playerAddPropertyArraySignature) ++
